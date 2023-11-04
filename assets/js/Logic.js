d653ece459054fece39326251fc1caa7f875ca87
Suhaim Khalid <suhaimkhalid007@gmail.com>
2023-11-04 16:23:52 +0000

    Complete code to start the quiz

--- assets/js/Logic.js
+++ assets/js/Logic.js
@@ -1,68 +1,72 @@
 
+// Delcare a varaible to the id of button to start quiz
 var start_quiz = document.querySelector("#start");
+// Delcare a variable to get the div of start screen
 var start_screen = document.querySelector("#start-screen");
-
+// Delcare a variable to get the div of Question screen
 var question_screen = document.querySelector("#questions");
+// Delcare a variable to get the h2 from question section
+var question_title = document.querySelector("#question-title");
+// Delcare a variable to show option of answers
+var choices = document.querySelector("#choices");
+// Delcare a variable to show time countdown
+var time = document.querySelector("#time");
 
-    var question_title = document.querySelector("#question-title");
+var count=75; //Declare a variable to have number valur that can be shown on the screen
+var qn=0;
 
-    var choices = document.querySelector("#choices");
+// start quiz
+start_quiz.addEventListener("click",function()
+{
+document.getElementById("start-screen").className = "hide";
+document.getElementById('questions').className = "";
+timerfunction();
+questiondisplay();
 
-    var time = document.querySelector("#time");
-    var count=40;
-    var qn=0;
-    // start quiz
-    start_quiz.addEventListener("click",function()
+        
+});
+
+
+function questiondisplay(){
+
+
+question_title.textContent = questions[qn].question;
+for(var i=0; i<questions[qn].answers.length; i++)
+{
+    var button = document.createElement("button");
+    choices.appendChild(button);
+    if(button.length>4)
     {
-        document.getElementById("start-screen").className = "hide";
-        document.getElementById('questions').className = "";
-        timerfunction();
-        questiondisplay();
-       
-             
-    });
+        button.remove();
+    } 
+    button.textContent= i+1 +"."+ questions[0].answers[i];    
+}
+}
 
 
-    function questiondisplay(){
-     
-       
-        question_title.textContent = questions[qn].question;
-        for(var i=0; i<questions[qn].answers.length; i++)
+
+function timerfunction (){
+
+var timer = setInterval(function(){
+    time.textContent=count;
+    count--;
+    if(count<0)
+    {
+        if(qn>questions.length-2)
         {
-            var button = document.createElement("button");
-            choices.appendChild(button);
-            if(button.length>4)
-            {
-                button.remove();
-            } 
-            button.textContent= i+1 +"."+ questions[0].answers[i];    
+        clearInterval(timer);
         }
+        else{
+        count=40;
+        qn++;
+        questiondisplay();
+        
     }
 
-
-
-    function timerfunction (){
-
-        var timer = setInterval(function(){
-            time.textContent=count;
-            count--;
-            if(count<0)
-            {
-               if(qn>questions.length-2)
-               {
-                clearInterval(timer);
-               }
-               else{
-                count=40;
-                qn++;
-                questiondisplay();
-               
-            }
-
-            }
-            else{
-
-            }
-        },100);
+    }
+    else{
 
     }
+},100);
+
+}
